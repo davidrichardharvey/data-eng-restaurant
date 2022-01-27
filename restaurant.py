4ed2c5ffbbc3be8d3c2b89a48a98895371057c36
ZF <zfarik@spartaglobal.com>
2022-01-27 21:08:27 +0000

    fixed restaurant by letting remove function take items completely off the bill, and by rounding up split_bill to the nearest penny

--- restaurant.py
+++ restaurant.py
@@ -1,3 +1,4 @@
+from math import ceil
 class Table:
     def __init__(self, diners):
         self.diners = int(diners)
@@ -12,9 +13,12 @@ class Table:
 
     def remove(self, foodstuff, price, quantity=1):
         for dictionaries in self.bill:
-            if dictionaries.get('item') == foodstuff and dictionaries.get('price') == price and dictionaries.get('quantity') >= quantity:
-                dictionaries['quantity'] -= quantity
-                return True
+            if dictionaries.get('item') == foodstuff and dictionaries.get('price') == price:
+                if dictionaries.get('quantity') <= quantity:
+                    self.bill.remove(dictionaries)
+                    return True
+                else:
+                    dictionaries['quantity'] -= quantity
         return False
 
     def get_subtotal(self):
@@ -23,8 +27,8 @@ class Table:
             subtotal += dictionaries.get('price') * dictionaries.get('quantity')
         return float(round(subtotal, 2))
 
-    def get_total(self, service_charge_percent):
+    def get_total(self, service_charge_percent=0.1):
         return {'Sub Total': f"£{self.get_subtotal():.2f}", 'Service Charge': f"£{self.get_subtotal() * service_charge_percent:.2f}", 'Total': f"£{self.get_subtotal() * (1+service_charge_percent):.2f}"}
 
     def split_bill(self):
-        return self.get_subtotal()/6
+        return ceil(100 * self.get_subtotal())/600
