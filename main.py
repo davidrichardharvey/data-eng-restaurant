8f13bff2b964b47163a2e432c8f8fd62a96080af
AhmedElawwad <ed.elawwad@gmail.com>
2022-05-28 00:09:06 +0100

    Only testing the branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from restaurant import Table
 from unittest import main
-
+print("Ahmed test")
 table01 = Table(5)
 table01.order("Risotto", 12.50, 2)
 print(table01.bill)
@@ -10,6 +10,8 @@ print(table01.bill)
 print(table01.get_subtotal())
 print(table01.get_total(0.15))
 print(table01.split_bill())
+
+
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
