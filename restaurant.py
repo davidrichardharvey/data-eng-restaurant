6bca99f85b33dffa7cc42a66a965693599ca4ff5
konrad1208 <32593523+konrad1208@users.noreply.github.com>
2022-05-27 16:29:16 +0100

    fixed the methods not returning correct things

--- restaurant.py
+++ restaurant.py
@@ -3,17 +3,22 @@ class Table:
         self.people_count = people_count
         self.bill = []
 
-    # create dict for the item, hence append to bill list
+    # create dict for the item, if item/price exists: increase quantity, else append to bill list
     def order(self, item_name, price, quantity=1):
-        order_add = {'item': item_name, 'price': price, 'quantity': quantity}
-        self.bill.append(order_add)
+        exists, location = self.check_if_item_exist(item_name, price)
+        if exists:
+            new_quantity = self.bill[location]["quantity"] + quantity
+            self.bill[location] = {"item": item_name, "price": price, "quantity": new_quantity}
+        else:
+            order_add = {"item": item_name, "price": price, "quantity": quantity}
+            self.bill.append(order_add)
 
-    # split the bill into.... the number of the table??? (assume it's people count)
+    # split the bill by people count
     def split_bill(self):
-        return self.get_subtotal() / self.people_count
+        return round(self.get_subtotal() / self.people_count, 2)
 
     # create a dict with formatted items that show the totals
-    def get_total(self, tip_perc):
+    def get_total(self, tip_perc=0.1):
         subtotal = self.get_subtotal()
         total = {"Sub Total": f"£{subtotal:>.2f}",
                  "Service Charge": f"£{subtotal * tip_perc:>.2f}",
@@ -27,9 +32,29 @@ class Table:
             subtotal += each_item["price"] * each_item["quantity"]
         return subtotal
 
-    # find the item, hence adjust quantity, hence stop searching
+    # adjust the item quantity, or remove it if <= 0
     def remove(self, item_name, price, quantity):
-        for i in self.bill:
-            if i["item"] == item_name and i["price"] == price:
-                i["quantity"] -= quantity
+        exists, location = self.check_if_item_exist(item_name, price)
+        if exists:
+            new_quantity = self.bill[location]["quantity"] - quantity
+            # reject the method if the result is negative quantity
+            if new_quantity < 0:
+                return False
+            # special case, remove the item if quantity set to 0
+            elif new_quantity == 0:
+                del self.bill[location]
+            else:
+                self.bill[location] = {"item": item_name, "price": price, "quantity": new_quantity}
+            return True
+        return False
+
+    # return if an item exists, and its location
+    def check_if_item_exist(self, item_name, price):
+        exists = False
+        location = -1
+        for index, value in enumerate(self.bill):
+            if value["item"] == item_name and value["price"] == price:
+                exists = True
+                location = index
                 break
+        return exists, location
